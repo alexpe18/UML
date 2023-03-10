41e0764911ee1f9d071b91c580b3a702108486d7
alexpe18 <106444850+alexpe18@users.noreply.github.com>
2023-03-10 12:57:25 -0600

    Add files via upload

---
+++ HTML/index.js
@@ -0,0 +1,117 @@
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+const LIST =[]
+
+let id = 0 // para que inicie en 0 cada tarea tendra un id diferente
+
+//creacion de fecha actualizada 
+
+const FECHA = new Date ()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday: 'long', month: 'short', day:'numeric'})
+
+
+
+
+
+
+
+// funcion de agregar tarea 
+
+function agregarTarea( tarea,id,realizado,eliminado) {
+    if(eliminado) {return} // si existe eliminado es true si no es false 
+
+    const REALIZADO = realizado ? check : uncheck // si realizado es verdadero check si no uncheck
+
+    const LINE = realizado ? lineThrough : '' 
+
+    const elemento = `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                    `
+    lista.insertAdjacentHTML("beforeend",elemento)
+
+}
+
+
+// funcion de Tarea Realizada 
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true //Si
+   // console.log(LIST)
+   // console.log(LIST[element.id])
+   // console.log(LIST[element.id].realizado)
+}
+
+function tareaEliminada(element){
+   // console.log(element.parentNode)
+   // console.log(element.parentNode.parentNode)
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+    console.log(LIST)
+}
+
+
+
+
+
+// crear un evento para escuchar el enter y para habilitar el boton 
+
+botonEnter.addEventListener('click', ()=> {
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        id++
+        input.value = ''
+    }
+
+})
+
+document.addEventListener('keyup', function (event) {
+    if (event.key=='Enter'){
+        const tarea = input.value
+        if(tarea) {
+            agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        id++
+        input.value = ''
+        console.log(LIST)
+        }
+    }
+    
+})
+
+
+lista.addEventListener('click',function(event){
+    const element = event.target 
+    const elementData = element.attributes.data.value
+    
+    if(elementData == 'realizado') {
+        tareaRealizada(element)
+    }
+    else if(elementData == 'eliminado') {
+        tareaEliminada(element)
+    }
+})
